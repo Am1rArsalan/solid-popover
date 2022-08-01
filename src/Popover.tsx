3370875d2f5902d04ba0a3d4703afc42c648493a
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-08-01 11:46:27 +0430

    wip: positioning popover

--- src/Popover.tsx
+++ src/Popover.tsx
@@ -5,7 +5,7 @@ import {
   ParentProps,
   Show,
   children,
-  createEffect,
+  createRenderEffect,
 } from "solid-js";
 import { createStore } from "solid-js/store";
 import {
@@ -21,28 +21,22 @@ import { Constants } from "./constants";
 import { createPopover } from "./createPopover";
 
 export const Popover: Component<ParentProps<PopoverProps>> = (props) => {
-  const {
-    containerClassName = "solid-tiny-popover",
-    onClickOutside,
-    positions = Constants.DEFAULT_POSITIONS,
-    align,
-    padding,
-    boundaryInset,
-    parentElement,
-    boundaryElement = parentElement,
-  } = props;
+  const { containerClassName = "solid-tiny-popover", onClickOutside } = props;
   let childRef: HTMLElement;
   const [popoverState, setPopoverState] = createStore<PopoverState>({
-    align,
+    align: props.align,
     nudgedLeft: 0,
     nudgedTop: 0,
-    position: positions[0],
-    padding,
+    position:
+      props.positions.length > 0
+        ? props.positions[0]
+        : Constants.DEFAULT_POSITIONS[0],
+    padding: props.padding,
     childRect: Constants.EMPTY_CLIENT_RECT,
     popoverRect: Constants.EMPTY_CLIENT_RECT,
     parentRect: Constants.EMPTY_CLIENT_RECT,
     boundaryRect: Constants.EMPTY_CLIENT_RECT,
-    boundaryInset,
+    boundaryInset: props.boundaryInset,
     violations: Constants.EMPTY_CLIENT_RECT,
     hasViolations: false,
   });
@@ -58,15 +52,17 @@ export const Popover: Component<ParentProps<PopoverProps>> = (props) => {
 
   const { popoverRef, scoutRef, positionPopover } = createPopover({
     isOpen: props.isOpen,
-    parentElement,
+    parentElement: props.parentElement,
     childRef,
     containerClassName,
-    boundaryElement,
+    boundaryElement: props.boundaryElement
+      ? props.boundaryElement
+      : props.parentElement,
     contentLocation: props.contentLocation,
-    positions,
-    align,
-    padding,
-    boundaryInset,
+    positions: props.positions ? props.positions : Constants.DEFAULT_POSITIONS,
+    align: props.align,
+    padding: props.padding,
+    boundaryInset: props.boundaryInset,
     reposition: props.reposition,
     onPositionPopover,
   });
@@ -93,62 +89,62 @@ export const Popover: Component<ParentProps<PopoverProps>> = (props) => {
     window.removeEventListener("resize", handleWindowResize);
   });
 
-  //const [shouldUpdate, setShouldUpdate] = createSignal(true);
   let shouldUpdate = true;
 
-  createEffect(() => {
-    console.log("call updatePopover", props.isOpen);
-    const updatePopover = () => {
-      console.log("in updatePopover", props.isOpen, shouldUpdate);
-      if (props.isOpen && shouldUpdate) {
-        const childRect = childRef?.getBoundingClientRect();
-        const popoverRect = popoverRef?.getBoundingClientRect();
-        if (
-          childRect != null &&
-          popoverRect != null &&
-          (!rectsAreEqual(childRect, {
-            top: popoverState.childRect.top,
-            left: popoverState.childRect.left,
-            width: popoverState.childRect.width,
-            height: popoverState.childRect.height,
-            bottom: popoverState.childRect.top + popoverState.childRect.height,
-            right: popoverState.childRect.left + popoverState.childRect.width,
-          } as DOMRect) ||
-            popoverRect.width !== popoverState.popoverRect.width ||
-            popoverRect.height !== popoverState.popoverRect.height ||
-            popoverState.padding !== padding ||
-            popoverState.align !== align ||
-            positions !== prevPositions ||
-            props.contentLocation !== prevContentLocation ||
-            props.reposition !== prevReposition)
-        ) {
-          positionPopover();
-        }
+  const updatePopover = () => {
+    console.log("in updatePopover", props.isOpen, shouldUpdate);
+    const childRect = childRef?.getBoundingClientRect();
+    const popoverRect = popoverRef?.getBoundingClientRect();
+    if (
+      childRect != null &&
+      popoverRect != null &&
+      (!rectsAreEqual(childRect, {
+        top: popoverState.childRect.top,
+        left: popoverState.childRect.left,
+        width: popoverState.childRect.width,
+        height: popoverState.childRect.height,
+        bottom: popoverState.childRect.top + popoverState.childRect.height,
+        right: popoverState.childRect.left + popoverState.childRect.width,
+      } as DOMRect) ||
+        popoverRect.width !== popoverState.popoverRect.width ||
+        popoverRect.height !== popoverState.popoverRect.height ||
+        popoverState.padding !== props.padding ||
+        popoverState.align !== props.align ||
+        props.positions !== prevPositions ||
+        props.contentLocation !== prevContentLocation ||
+        props.reposition !== prevReposition)
+    ) {
+      positionPopover();
+    }
 
-        // TODO: factor prev checks out into the custom prev....s hook
-        if (positions !== prevPositions) {
-          prevPositions = positions;
-        }
-        if (props.contentLocation !== prevContentLocation) {
-          prevContentLocation = props.contentLocation;
-        }
-        if (props.reposition !== prevReposition) {
-          prevReposition = props.reposition;
-        }
+    // TODO: factor prev checks out into the custom prev....s hook
+    if (props.positions !== prevPositions) {
+      prevPositions = props.positions;
+    }
+    if (props.contentLocation !== prevContentLocation) {
+      prevContentLocation = props.contentLocation;
+    }
+    if (props.reposition !== prevReposition) {
+      prevReposition = props.reposition;
+    }
 
-        if (shouldUpdate) {
-          // why we are not here ....
-          console.log("requestAnimationFrame");
-          window.requestAnimationFrame(updatePopover);
-        }
-      }
+    // I don't know what the fuck is going on here
+    //if (shouldUpdate) {
+    //window.requestAnimationFrame(updatePopover);
+    //prevIsOpen = props.isOpen;
+    //}
+  };
 
+  createRenderEffect(() => {
+    console.log("call updatePopover");
+    if (props.isOpen && shouldUpdate) {
+      window.requestAnimationFrame(updatePopover);
       prevIsOpen = props.isOpen;
-    };
-    window.requestAnimationFrame(updatePopover);
+    }
   });
 
   onCleanup(() => {
+    console.log("In clean up");
     shouldUpdate = false;
   });
 
@@ -164,7 +160,7 @@ export const Popover: Component<ParentProps<PopoverProps>> = (props) => {
       <Show when={props.isOpen}>
         <PopoverPortal
           element={popoverRef}
-          container={parentElement}
+          container={props.parentElement}
           scoutElement={scoutRef}
         >
           {typeof props.content !== "function" && props.content}

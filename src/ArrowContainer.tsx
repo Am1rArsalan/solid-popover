646c9bf9e6684df4cb7b97fc60259c013939ce82
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-08-13 01:45:02 +0430

    feat: add arrowContainer component

---
+++ src/ArrowContainer.tsx
@@ -0,0 +1,40 @@
+import { ArrowContainerProps } from "./types";
+import { createArrowContainer } from "./createArrowContainer";
+
+export function ArrowContainer(props: ArrowContainerProps) {
+  const {
+    childRect,
+    popoverRect,
+    position,
+    arrowColor,
+    arrowSize,
+    arrowClassName,
+    arrowStyle: externalArrowStyle,
+    className,
+    style: externalArrowContainerStyle,
+  } = props;
+  const { arrowContainerStyle, arrowStyle } = createArrowContainer({
+    childRect,
+    popoverRect,
+    position,
+    arrowColor,
+    arrowSize,
+  });
+
+  const mergedContainerStyle = {
+    ...arrowContainerStyle,
+    ...externalArrowContainerStyle,
+  };
+
+  const mergedArrowStyle = {
+    ...arrowStyle,
+    ...externalArrowStyle,
+  };
+
+  return (
+    <div class={className} style={mergedContainerStyle}>
+      <div style={mergedArrowStyle} class={arrowClassName} />
+      {props.children}
+    </div>
+  );
+}
